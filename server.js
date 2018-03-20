85ea5a919bf8de65303fc66d809b32104f58dc5e
ssheaks <sarah.sheaks@gmail.com>
2018-03-20 10:10:47 -0700

    initial commit after clone

--- server.js
+++ server.js
@@ -18,25 +18,44 @@ app.use(bodyParser.json());
 
 // GET requests to /restaurants => return 10 restaurants
 app.get('/restaurants', (req, res) => {
+  const filters = {};
+  const queryableFields = ['cuisine', 'borough'];
+  queryableFields.forEach(field => {
+    if (req.query[field]) {
+      filters[field] = req.query[field];
+    }
+  });
   Restaurant
-    .find()
-    // we're limiting because restaurants db has > 25,000
-    // documents, and that's too much to process/return
-    .limit(10)
-    // success callback: for each restaurant we got back, we'll
-    // call the `.serialize` instance method we've created in
-    // models.js in order to only expose the data we want the API return.
-    .then(restaurants => {
-      res.json({
-        restaurants: restaurants.map(
-          (restaurant) => restaurant.serialize())
-      });
-    })
+    .find(filters)
+    .then(Restaurants => res.json(
+      Restaurants.map(restaurant => restaurant.serialize())
+    ))
     .catch(err => {
       console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
+      res.status(500).json({message: 'Internal server error'});
     });
 });
+
+// app.get('/restaurants', (req, res) => {
+//   Restaurant
+//     .find()
+//     // we're limiting because restaurants db has > 25,000
+//     // documents, and that's too much to process/return
+//     .limit(10)
+//     // success callback: for each restaurant we got back, we'll
+//     // call the `.serialize` instance method we've created in
+//     // models.js in order to only expose the data we want the API return.
+//     .then(restaurants => {
+//       res.json({
+//         restaurants: restaurants.map(
+//           (restaurant) => restaurant.serialize())
+//       });
+//     })
+//     .catch(err => {
+//       console.error(err);
+//       res.status(500).json({ message: 'Internal server error' });
+//     });
+// });
 
 // can also request by ID
 app.get('/restaurants/:id', (req, res) => {
